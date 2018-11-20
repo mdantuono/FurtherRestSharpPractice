30a517d9e89b633e152f4083b9def317f3d3c82a
mdantuono <mikeishere3@gmail.com>
2018-11-20 13:47:17 -0500

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string status = "This tweet was posted using RestSharp.";
+            var client = new RestClient("https://api.twitter.com/1.1");
+            var request = new RestRequest("/statuses/update", Method.POST);
+            request.AddParameter("status", status);
+
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content;
+            Console.Write(content);
+        }
+    }
+}
