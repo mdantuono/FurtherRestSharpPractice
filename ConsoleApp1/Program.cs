6f3f1f21277fe809863478557bff2377179cce31
mdantuono <mikeishere3@gmail.com>
2018-12-04 18:29:58 -0500

    changed endpoint and added headers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,11 +14,10 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             string status = "This tweet was posted using RestSharp.";
-            var client = new RestClient("https://twitter.com");
-            var request = new RestRequest("/statuses/update", Method.POST);
-            request.AddParameter("name", "Mike_DAntuono");
+            var client = new RestClient("https://api.twitter.com/");
+            var request = new RestRequest("1.1/statuses/update.json?", Method.POST);
+            request.AddHeader("Authorization", "OAuth oauth_consumer_key='2WNGB1TGcoZtMgIe7A5aFloHV',oauth_token='239156828 - hRlujnJQLZMbX3zTDRuRUP0ItXKUrzqCMC8UhhZw',oauth_signature_method='HMAC - SHA1'");
             request.AddParameter("status", status);
-
 
             IRestResponse response = client.Execute(request);
             var content = response.Content;
