d6b7055681b3d3ab099ebd2e966ed78017ece29a
mdantuono <mikeishere3@gmail.com>
2018-12-05 13:49:22 -0500

    added some variables and git ignore

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -16,8 +16,16 @@ namespace ConsoleApp1
             string status = "This tweet was posted using RestSharp.";
             var client = new RestClient("https://api.twitter.com/");
             var request = new RestRequest("1.1/statuses/update.json?", Method.POST);
-            request.AddHeader("Authorization", "OAuth oauth_consumer_key");
+            request.AddHeader("Authorization", "OAuth oauth_consumer_key = " + Keys.consumerKey + ", oauth_token = " + Keys.accessToken);
             request.AddParameter("status", status);
+
+            //OAuth oauth_consumer_key = "2WNGB1TGcoZtMgIe7A5aFloHV", 
+            //    oauth_token = "239156828-hRlujnJQLZMbX3zTDRuRUP0ItXKUrzqCMC8UhhZw", 
+            //    oauth_signature_method = "HMAC-SHA1", 
+            //    oauth_timestamp = "1543953553", 
+            //    oauth_nonce = "51E9bN0bDAK", 
+            //    oauth_version = "1.0", 
+            //    oauth_signature = "ALOwjTSL2WmG%2FeDcuSJU2vVcWTk%3D"
 
 
             IRestResponse response = client.Execute(request);
